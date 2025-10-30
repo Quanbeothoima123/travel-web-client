2f86b9385f3dd8f4f88d1a0be84d9b49f92b09b9
Quanbeothoima123 <phimanhh85@gmail.com>
2025-10-30 14:01:08 +0700

    Cập nhật next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.vietnamtravelgroup.com.vn",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "vietnamtravelgroup.com.vn",
-        port: "",
-        pathname: "/**",
+        hostname: "**", // Cho phép TẤT CẢ hostname với https
       },
       {
         protocol: "http",
-        hostname: "localhost",
-        port: "8080",
-        pathname: "/**",
+        hostname: "**", // Cho phép TẤT CẢ hostname với http
       },
     ],
   },
