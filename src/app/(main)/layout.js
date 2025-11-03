6bccd795d76ea87d77f7978e94b30102a0ca97ad
Quanbeothoima123 <phimanhh85@gmail.com>
2025-11-03 16:55:44 +0700

    tạo layout.js cho (main)

---
+++ src/app/(main)/layout.js
@@ -0,0 +1,16 @@
+// app/(main)/layout.js
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+import WeatherWidget from "@/components/common/WeatherWidget";
+import ContactFloating from "@/components/common/ContactFloating";
+export default function MainLayout({ children }) {
+  return (
+    <>
+      <Header />
+      <main className="pt-16">{children}</main>
+      <Footer />
+      <WeatherWidget />
+      <ContactFloating />
+    </>
+  );
+}
