8e4f3df2aa05b4f2726e922bb0047af22a50dc28
Quanbeothoima123 <phimanhh85@gmail.com>
2025-11-03 17:00:13 +0700

    Cập nhật cấu trúc file

--- src/app/booking-tour/[slug]/layout.js
+++
@@ -1,9 +0,0 @@
-// app/booking-tour/[slug]/layout.js
-export const metadata = {
-  title: "Đặt Tour",
-  description: "Đặt tour du lịch trực tuyến",
-};
-
-export default function BookingLayout({ children }) {
-  return children;
-}
